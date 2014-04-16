095fa5a58ddf0283764345b31b59478ca559f9f5
freek022 <fkg333_09@icloud.com>
2014-04-16 14:07:48 +0100

    new db file

--- app/Config/database.php
+++ app/Config/database.php
@@ -61,16 +61,7 @@
  */
 class DATABASE_CONFIG {
 
-/* 	public $default = array(
-		'datasource' => 'Database/Mysql',
-		'persistent' => false,
-		'host' => 'localhost',
-		'login' => 'root',
-		'password' => '',
-		'database' => 'cakeblog',
-		'prefix' => '',
-		'encoding' => 'utf8',
-	) */;
+
 
 	public $default = array(
 		'datasource' => 'Database/Mysql',
