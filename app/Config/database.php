48c6df2faa645da2fcf90f40b652255fdd5de583
freek022 <fkg333_09@icloud.com>
2014-04-16 15:39:12 +0100

    made changes

--- app/Config/database.php
+++ app/Config/database.php
@@ -61,8 +61,17 @@
  */
 class DATABASE_CONFIG {
 
-
-
+	public $default = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'root',
+		'password' => '',
+		'database' => 'cakeblog',
+		'prefix' => '',
+		'encoding' => 'utf8',
+	);
+/* 
 	public $default = array(
 		'datasource' => 'Database/Mysql',
 		'persistent' => false,
@@ -73,6 +82,6 @@ class DATABASE_CONFIG {
 		'prefix' => '',
 		//'encoding' => 'utf8',
 	);
-	
+	 */
 	
 }
